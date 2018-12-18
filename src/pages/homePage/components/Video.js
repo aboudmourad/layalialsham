4f8d09200058a9e34bde51bd47ac325d7e56ac33
abboudmourad <aboudmourad@hotmail.com>
2018-12-18 15:05:30 +0100

    fix style responsive

--- src/pages/homePage/components/Video.js
+++ src/pages/homePage/components/Video.js
@@ -8,9 +8,10 @@ const Video = () => {
   }
   return (
     <div className="facebookVideo">
-    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F54alyalialsham%2Fvideos%2F1538494756191946%2F&show_text=0&width=560&height=400" 
-    width="560" height="400" style={style} scrolling="no" frameBorder="0" allowtransparency="true" allowFullScreen={true} title="video"
-    webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+      <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F54alyalialsham%2Fvideos%2F1538494756191946%2F&show_text=0&width=560&height=400" 
+      width="560" height="400" style={style} scrolling="no" frameBorder="0" allowtransparency="true" allowFullScreen={true} title="video"
+      webkitallowfullscreen="true" mozallowfullscreen="true">
+      </iframe>
 	</div>
  
   )
